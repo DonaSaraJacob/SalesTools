f24b402bedc48e455eacf428f1d24af9dae13a88
DonaSaraJacob <54465841+DonaSaraJacob@users.noreply.github.com>
2019-09-23 16:05:02 +0530

    Initial COmmit

---
+++ src/com/ust/faith/Main.java
@@ -0,0 +1,14 @@
+package com.ust.faith;
+public class Main {
+	public static void main(String[] args) {
+		Sales objSalesData = new Sales();
+		System.out.println("Hello Ust guys! Git Repo");
+		displayGreetings();
+		objSalesData.display();
+	}
+	private static void displayGreetings() {
+		System.out.println();
+
+	}
+
+}
---
+++ src/com/ust/faith/Sales.java
@@ -0,0 +1,8 @@
+package com.ust.faith;
+
+public class Sales {
+	void display() {
+		System.out.println("Welcome to sales report app 2019");
+
+	}
+}
