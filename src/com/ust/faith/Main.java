d8b272279bcc32c737e95fc115b44f326472af83
DonaSaraJacob <54465841+DonaSaraJacob@users.noreply.github.com>
2019-09-23 16:12:14 +0530

    Second Commit

--- src/com/ust/faith/Main.java
+++ src/com/ust/faith/Main.java
@@ -2,7 +2,7 @@ package com.ust.faith;
 public class Main {
 	public static void main(String[] args) {
 		Sales objSalesData = new Sales();
-		System.out.println("Hello UST guys! Git Repo");
+		System.out.println("Hello UST Guys! Git Repo");
 		displayGreetings();
 		objSalesData.display();
 	}
